f5cd179bd0af009f3cd2576d7e5598c2c09b53c4
Carlos Valiente <carlos@pepelabs.net>
2018-10-10 18:59:30 +0100

    Use storage pools.

--- x.py
+++ x.py
@@ -31,22 +31,17 @@ class Volume(object):
         self.pool = pool
 
     def upload(self, src):
-        pool = self.pool.pool
-        size = os.stat(src).st_size
         try:
-            vol = pool.storageVolLookupByName(self.name)
+            self.pool.storageVolLookupByName(self.name).delete()
         except libvirt.libvirtError as err:
             if err.get_error_code() != libvirt.VIR_ERR_NO_STORAGE_VOL:
                 raise
 
-            vol = pool.createXML(VOLUME_XML_TEMPLATE.format(name=self.name,
-                                                            capacity=size))
-        else:
-            _, _, allocated = vol.info()
-            if size > allocated:
-                vol.resize(size, flags=libvirt.VIR_STORAGE_VOL_RESIZE_SHRINK)
+        size = os.stat(src).st_size
+        vol = self.pool.createXML(VOLUME_XML_TEMPLATE.format(name=self.name,
+                                                             capacity=size))
 
-        stream = self.pool.conn.newStream()
+        stream = self.pool.connect().newStream()
         vol.upload(stream, 0, size, flags=0)
         with open(src, 'rb') as f:
             while True:
@@ -63,13 +58,12 @@ class Pool(object):
     def __init__(self, name, path):
         self.name = name
         self.path = path
-        self._conn = None
         self._pool = None
 
     def create(self):
-        self._conn = libvirt.open()
+        c = libvirt.open()
         try:
-            pool = self._conn.storagePoolLookupByName(self.name)
+            pool = c.storagePoolLookupByName(self.name)
         except libvirt.libvirtError as err:
             if err.get_error_code() != libvirt.VIR_ERR_NO_STORAGE_POOL:
                 raise
@@ -78,7 +72,7 @@ class Pool(object):
                 os.makedirs(self.path)
 
             pool_xml = POOL_XML_TEMPLATE.format(name=self.name, path=self.path)
-            pool = self._conn.storagePoolDefineXML(pool_xml, flags=0)
+            pool = c.storagePoolDefineXML(pool_xml, flags=0)
             pool.build(libvirt.VIR_STORAGE_POOL_BUILD_NEW)
 
         if not pool.isActive():
@@ -99,14 +93,12 @@ class Pool(object):
         self.pool.undefine()
 
     def upload(self, src, dest):
-        v = Volume(dest, self)
+        v = Volume(dest, self.pool)
         v.upload(src)
 
     def _free(self):
         if self._pool is not None:
             self._pool = None
-        if self._conn is not None:
-            self._conn = None
 
     @property
     def pool(self):
@@ -114,28 +106,25 @@ class Pool(object):
             raise Exception('Pool {} not initialized'.format(self))
         return self._pool
 
-    @property
-    def conn(self):
-        if self._conn is None:
-            raise Exception('Pool {} not initialized'.format(self))
-        return self._conn
 
-
-def upload(src, pool, pool_home):
+def upload(src, pool, pool_home, dest):
     path = os.path.join(os.path.expanduser(pool_home), pool)
+    if dest is None:
+        dest = os.path.basename(src)
     with Pool(pool, path) as p:
-        p.upload(src, os.path.basename(src))
+        p.upload(src, dest)
 
 
 def main():
     p = argparse.ArgumentParser()
     p.add_argument('src')
     p.add_argument('pool')
+    p.add_argument('--dest')
     p.add_argument('--pool-home', default='~/.libvirt/pools')
 
     args = p.parse_args()
 
-    return upload(args.src, args.pool, args.pool_home)
+    return upload(args.src, args.pool, args.pool_home, args.dest)
 
 
 if __name__ == '__main__':
